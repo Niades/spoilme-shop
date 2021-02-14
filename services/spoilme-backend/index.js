bbd8b274e6df7ed1c89bcd3080723e442a536a19
Daniil Polovinkin <dolovinkin@gmail.com>
2021-02-15 00:35:39 +0300

    cors

--- services/spoilme-backend/index.js
+++ services/spoilme-backend/index.js
@@ -5,12 +5,27 @@ const app = fastify({
   logger: true,
 });
 
+app.register(require("fastify-cors"), {
+  origin: "*"
+});
+
 const getUserOpts = {
   schema: {
     querystring: {
       type: "object",
       properties: {
         username: { type: "string" }
+      },
+      required: [ "username" ]
+    }
+  },
+  response: {
+    201: {
+      type: 'object',
+      properties: {
+        id: { type: 'number' },
+        username: { type: 'string' },
+        profileDescription: { type: 'string' }
       }
     }
   }
@@ -20,7 +35,7 @@ app.get('/api/v1/user', getUserOpts, function(request) {
   return db.models.User.findOne({
     where: { username: request.query.username },
     attributes: {
-      exclude: ['firstName', 'lastName'],
+      exclude: ['firstName', 'lastName', 'updatedAt', 'createdAt'],
     },
   })
 });
