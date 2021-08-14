e3089c59dd332e25fbb34cd1f458c1a5a3e7d44b
Daniil Polovinkin <dolovinkin@gmail.com>
2021-08-14 09:39:30 +0300

    staging should be on *-test <_<

--- services/spoilme-backend/constants.js
+++ services/spoilme-backend/constants.js
@@ -1,10 +1,4 @@
-const PROD = process.env.NODE_ENV === "production";
-let DB_CONNECTION_STRING;
-if(PROD) {
-  DB_CONNECTION_STRING = "postgres://postgres:postgres@db:5432/spoilme";
-} else {
-  DB_CONNECTION_STRING = "sqlite:db.sqlite3";
-}
+const PROD = process.env.NODE_ENV === "production" && !process.env.STAGING;
 const FB_COLLECTIONS = {
   PRODUCTS: !PROD ? "products-test" : "products",
   USERS: !PROD ? "users-test" : "users",
@@ -13,5 +7,4 @@ const FB_COLLECTIONS = {
 module.exports = {
   PROD,
   FB_COLLECTIONS,
-  DB_CONNECTION_STRING,
 };
